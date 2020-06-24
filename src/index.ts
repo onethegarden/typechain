d59e133ea4f2c773a98589e0c7761c4327305556
onethegarden <onethegarden@gmail.com>
2020-06-25 07:00:51 +0900

    #6 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  ///?parameter is optional
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "nicolas",
+  age: 22,
+  gender: "male",
 };
 
-console.log(sayHi("Nicolas", 444, "male"));
+const sayHi = (person: Human): string => {
+  ///?parameter is optional
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 export {};
