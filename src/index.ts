28a969384d644cbd59e7bac36ab6842e24f6427c
onethegarden <onethegarden@gmail.com>
2020-06-27 11:07:57 +0900

    7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+
+const lynn = new Human("Lynn", 18, "female");
 
 const person = {
   name: "nicolas",
