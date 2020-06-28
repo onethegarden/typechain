6ecd63e467a446d5f29819a661a990db19094976
onethegarden <onethegarden@gmail.com>
2020-06-29 06:52:49 +0900

    #10 Creating a Block part Three

--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,26 @@ const getLatestBlock = (): Block => getBlockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
+const createNewBlock = (data: string): Block => {
+  const previosBlock: Block = getLatestBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previosBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previosBlock.hash,
+    data,
+    newTimestamp
+  );
+
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye"));
 export {};
